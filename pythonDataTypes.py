fe7f54e4b74b6f1d1854aaf6d389294de32ba656
Riyana Expedith <riyana.je@gmail.com>
2020-09-13 15:29:24 -0500

    just testing this

---
+++ pythonDataTypes.py
@@ -0,0 +1,16 @@
+# i = int(input("enter a number: "))
+# f = float(input("enter a decimal: "))
+# s = input("enter a text: ")
+my_friends = ['Ashriya', 'Sadhaya', 'Jhanvi']
+
+# print(i)
+# print(f)
+# print(s)
+# print(my_friends)
+
+# for name in my_friends:
+   # print(name)
+
+print(my_friends[0])
+print(my_friends[1])
+print(my_friends[2])
